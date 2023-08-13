10154336b03c9d1087376de951c25c7e05839fbe
Simone-Scalella <S1088431@studenti.univpm.it>
2023-08-13 18:52:38 +0200

    ok simo log 13_08_2023

--- experiments/obdf-resultAnalysis/src/ResultToCSVfull.java
+++ experiments/obdf-resultAnalysis/src/ResultToCSVfull.java
@@ -126,7 +126,7 @@ public class ResultToCSVfull {
     public static void main(String[] args){
 
         ResultToCSVfull RTA = new ResultToCSVfull();
-        String[] data = {"2000", "200000", "2000000"};
+        String[] data = {"20000", "200000", "2000000"};
 
         for (String i : data) {
             RTA.format1ToCSV("./obda-mixer/results/"+i+"/","./obdf-resultAnalysis/resultFiles/"+i+".csv");
